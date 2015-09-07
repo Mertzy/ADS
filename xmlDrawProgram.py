8eb1fa6e36632bc6ec18aa3a5ec671fc359ead8a
Isaac Joseph Mertzenich <mertis01@8274.local>
2015-09-06 22:24:57 -0500

    homework changes

--- xmlDrawProgram.py
+++ xmlDrawProgram.py
@@ -31,6 +31,28 @@ class GoToCommand:
         return '<Command x="' + str(self.x) + '" y="' + str(self.y) + \
                '" width="' + str(self.width) \
                + '" color="' + self.color + '">GoTo</Command>' 
+
+class SquareCommand:
+    def __init__(self,length=1, width=1,color="black"):
+        self.length = length
+        self.width = width
+        self.color = color
+        
+    def draw(self,turtle):
+        turtle.width(self.width)
+        turtle.pencolor(self.color)
+        turtle.forward(self.length)
+        turtle.right(90)
+        turtle.forward(self.length)
+        turtle.right(90)
+        turtle.forward(self.length)
+        turtle.right(90)
+        turtle.forward(self.length)
+        turtle.right(90)
+        
+    def __str__(self):
+        return '<Command length="' + str(self.length) + '" width="' + \
+               str(self.width) + '" color="' + self.color + '">Square</Command>'
         
 class CircleCommand:
     def __init__(self,radius, width=1,color="black"):
@@ -176,6 +198,13 @@ class DrawingApplication(tkinter.Frame):
                     width = float(attr["width"].value)
                     color = attr["color"].value.strip()
                     cmd = GoToCommand(x,y,width,color)
+
+                elif command == "Circle":
+                    length = float(attr["length"].value)
+                    width = float(attr["width"].value)
+                    color = attr["color"].value.strip()
+                    cmd = SquareCommand(length,width,color)
+        
         
                 elif command == "Circle":
                     radius = float(attr["radius"].value)
@@ -311,6 +340,15 @@ class DrawingApplication(tkinter.Frame):
         widthEntry = tkinter.Entry(sideBar,textvariable=widthSize)
         widthEntry.pack()
         widthSize.set(str(1))
+
+        lengthSize = tkinter.StringVar()
+        lengthEntry = tkinter.Entry(sideBar,textvariable=lengthSize)
+        lengthEntry.pack()
+        lengthSize.set(str(1))
+
+        lengthLabel = tkinter.Label(sideBar,text="Length")
+        lengthLabel.pack()
+
         
         radiusLabel = tkinter.Label(sideBar,text="Radius")
         radiusLabel.pack()
@@ -321,6 +359,18 @@ class DrawingApplication(tkinter.Frame):
         
         # A button widget calls an event handler when it is pressed. The circleHandler
         # function below is the event handler when the Draw Circle button is pressed. 
+
+        def squareHandler():
+            cmd = SquareCommand(float(lengthSize.get()), float(widthSize.get()), penColor.get())
+            cmd.draw(theTurtle)
+            self.graphicsCommands.append(cmd)
+            
+            screen.update()
+            screen.listen()
+
+        squareButton = tkinter.Button(sideBar, text = "Draw Square (Only alter length attribute)", command=squareHandler)
+        squareButton.pack(fill=tkinter.BOTH)     
+
         def circleHandler():
             # When drawing, a command is created and then the command is drawn by calling
             # the draw method. Adding the command to the graphicsCommands sequence means the
